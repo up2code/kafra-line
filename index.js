762322c6c0ee01e8920cd50cb2b5c95756e555bf
Augsorn Chanklad <augsorn.cha@ascendcorp.com>
2019-02-21 18:57:39 +0700

    fix callback

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 'use strict';
 
 const lineConfig = require('./src/line.config');
-const webhookHandler = require('./src/webhook.handler');
+const eventMessageHandler = require('./event-message.handler');
 const express = require('express');
 
 const app = express();
 
-app.post('/callback', lineConfig.middleware, webhookHandler);
+app.post('/callback', lineConfig.middleware, (req, res) => {
+  req.body.events.map(event => {
+      if(event != 'message' || event.message.type != 'text') {
+          return;
+      }
+
+      console.log(event.message.text);
+
+      return eventMessage(event.message.text, message => {
+          console.log('Reply :' + JSON.stringify(message));
+          lineConfig.client.replyMessage(event.replyToken, message);
+      })
+  });
+  res.sendStatus(200)
+});
 
 app.get('/',(req, res) => {
     res.send('hello kafra')
