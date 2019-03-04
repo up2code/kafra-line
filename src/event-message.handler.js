5277c9464a4001c4d35cf2254608da04d86e9e41
Augsorn Chanklad <augsorn.cha@ascendcorp.com>
2019-03-04 13:49:26 +0700

    Add handle gacha command

--- src/event-message.handler.js
+++ src/event-message.handler.js
@@ -7,18 +7,21 @@ const botMessage = require('./message.default');
 const format = require('string-format');
 const lineMessage = require('./line.message');
 const chat = require('./chat');
+const gacha = require('./gacha');
 
 const cmdTypeConst = {
     command: 'cmd',
     poporing: 'poporing',
     romexchange: 'romexchange',
-    chat: 'chat'
+    chat: 'chat',
+    gacha: 'gacha'
 }
 
 const getMessageCmdType = text => {
     if(text.startsWith('!')) return cmdTypeConst.command;
     if(text.startsWith('$$')) return cmdTypeConst.romexchange;
     if(text.startsWith('$')) return cmdTypeConst.poporing;
+    if(text.startsWith('ไขกาชา')) return cmdTypeConst.gacha;
     return cmdTypeConst.chat;
 }
 
@@ -59,6 +62,13 @@ module.exports = (text, callback) => {
             } else {
                 poporing.getLatestPrices(remainText, priceListResponse);
             }
+            break;
+        case cmdTypeConst.gacha:
+            gacha.pull().then(result => {
+                if(result) {
+                    callback(result);
+                }
+            });
             break;
         case cmdTypeConst.chat:
             chat(text).then(answerMessage => {
