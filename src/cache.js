33cf150fbec086fe4af133f452008c6efcaf9d1f
Augsorn Chanklad <augsorn.cha@ascendcorp.com>
2019-04-24 15:26:09 +0700

    Fix cache

--- src/cache.js
+++ src/cache.js
@@ -4,12 +4,29 @@ const myCache = new NodeCache({ stdTTL: 100, checkperiod: 120 });
 module.exports = {
     get: key => {
         console.log('Try get ' + key + ' from cache');
-        return new Promise(resolve => {
-            myCache.get(key, resolve);
+        return new Promise((resolve, reject) => {
+            myCache.get(key, (err, value) => {
+                if(err) { 
+                    console.error(err);
+                    reject(err);
+                } else {
+                    resolve(value);
+                }
+            });
         });
     },
     set: (key, value) => {
         console.log('Try set ' + key + ' to cache');
-        return myCache.set(key, value);
+
+        return new Promise((resolve,reject) => {
+            myCache.set(key, value, (err, success) => {
+                if(err) {
+                    console.error(err)
+                    reject(err);
+                } else {
+                    resolve(success);
+                }
+            });
+        });
     }
 }
