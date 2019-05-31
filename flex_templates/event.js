aad017f1f2d31af0010e9a765e19746a24191b1c
Augsorn Chanklad <augsorn.cha@ascendcorp.com>
2019-05-31 14:25:52 +0700

    Change event flex template format

--- flex_templates/event.js
+++ flex_templates/event.js
@@ -23,83 +23,85 @@ module.exports = generate = (event) => {
                "size":"md"
             },
             {
+    
+               "type": "box",
+               "layout": "vertical",
+               "contents": [
+                 {
+                   "type": "box",
+                   "layout": "vertical",
+                   "contents": [
+                     {
+                       "type": "box",
+                       "layout": "baseline",
+                       "spacing": "sm",
+                       "contents": [
+                         {
+                           "type": "text",
+                           "text": "Start",
+                           "weight": "bold",
+                           "size": "sm",
+                           "flex": 1
+                         },
+                         {
+                           "type": "text",
+                           "text": event.start,
+                           "wrap": true,
+                           "color": "#666666",
+                           "size": "sm",
+                           "flex": 5
+                         }
+                       ]
+                     }
+                   ]
+                 },
+                 {
+                   "type": "box",
+                   "layout": "vertical",
+                   "contents": [
+                     {
+                       "type": "box",
+                       "layout": "baseline",
+                       "spacing": "sm",
+                       "contents": [
+                         {
+                           "type": "text",
+                           "text": "End",
+                           "weight": "bold",
+                           "size": "sm",
+                           "flex": 1
+                         },
+                         {
+                           "type": "text",
+                           "text": event.end,
+                           "wrap": true,
+                           "color": "#666666",
+                           "size": "sm",
+                           "flex": 5
+                         }
+                       ]
+                     }
+                   ]
+                 },
+                 {
+                   "type": "box",
+                   "layout": "horizontal",
+                   "contents": [
+                     {
+                       "type": "text",
+                       "color": "#aaaaaa",
+                       "text": event.fromNow
+                     }
+                   ]
+                 }
+               ]
+             },
+            {
                "type":"text",
                "text": event.description,
+               "margin": "lg",
                "size":"sm",
                "wrap":true
-            },
-            {
-               "type":"box",
-               "layout":"vertical",
-               "margin":"lg",
-               "spacing":"sm",
-               "contents":[
-                  {
-                     "type":"box",
-                     "layout":"baseline",
-                     "spacing":"sm",
-                     "contents":[
-                        {
-                           "type":"text",
-                           "text":"Start",
-                           "weight":"bold",
-                           "size":"sm",
-                           "flex":1
-                        },
-                        {
-                           "type":"text",
-                           "text": event.start,
-                           "wrap":true,
-                           "color":"#666666",
-                           "size":"sm",
-                           "flex":5
-                        }
-                     ]
-                  }
-               ]
-            },
-            {
-               "type":"box",
-               "layout":"vertical",
-               "margin":"lg",
-               "spacing":"sm",
-               "contents":[
-                  {
-                     "type":"box",
-                     "layout":"baseline",
-                     "spacing":"sm",
-                     "contents":[
-                        {
-                           "type":"text",
-                           "text":"End",
-                           "weight":"bold",
-                           "size":"sm",
-                           "flex":1
-                        },
-                        {
-                           "type":"text",
-                           "text": event.end,
-                           "wrap":true,
-                           "color":"#666666",
-                           "size":"sm",
-                           "flex":5
-                        }
-                     ]
-                  }
-               ]
-            },
-            {
-               "type":"box",
-               "layout":"horizontal",
-               "margin":"lg",
-               "spacing":"sm",
-               "contents":[
-                  {
-                     "type":"text",
-                     "color":"#aaaaaa",
-                     "text": event.fromNow
-                  }
-               ]
             }
          ]
       }
