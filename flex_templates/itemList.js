b60e1a67c809acbe5242d040fab0af95448053e6
up2code <uppban5@gmail.com>
2019-06-18 22:31:28 +0700

    Fix item list use plain text for temporary

--- flex_templates/itemList.js
+++ flex_templates/itemList.js
@@ -1,9 +1,36 @@
 const itemTemplate = require('./item')
 
-module.exports = generate = (items) => {
-    return {
-        type: 'flex',
-        altText: 'Search result',
-        contents: { type: 'carousel', contents: items.map(i => itemTemplate(i, i.priceData))}
+/*
+ [
+   {
+      "name": "cyfar"
+      "display_name": "Cyfar",
+      "image_url": "https://via.placeholder.com/50x50?text=?",
+      "priceData": {
+        "price": 100,
+        "volume": 100,
+        "timestamp": 123456,
+        "change1day": 32.12,
+        "change3day": 32.12,
+        "change7day": 32.12,
       }
-};
+   }
+ ]
+*/
+
+const generateFlex = items => {
+  return {
+    type: 'flex',
+    altText: 'Search result',
+    contents: { type: 'carousel', contents: items.map(i => itemTemplate(i, i.priceData))}
+  }
+}
+
+const generatePlainText = items => {
+  return {
+    type: 'text',
+    text: items.map(i => `${i.display_name}: ${i.priceData.price}z`).join('\n')
+  }
+}
+
+module.exports = generatePlainText;
