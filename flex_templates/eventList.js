a72ee9425f0fd8867c0dbeb859950ea603b5a96e
up2code <uppban5@gmail.com>
2019-06-18 22:58:55 +0700

    Change event list flex to plain text for temporary

--- flex_templates/eventList.js
+++ flex_templates/eventList.js
@@ -1,9 +1,32 @@
 const eventTemplate = require('./event')
 
-module.exports = generate = (events) => {
-    return {
-        type: 'flex',
-        altText: 'กิจกรรม/Events',
-        contents: { type: 'carousel', contents: events.map(e => eventTemplate(e))}
-      }
-};
+/*
+[
+  {
+    "thumbUrl": "https://abc.png",
+    "detailUrl": "https://google.com",
+    "name": "Event name",
+    "start": "Start time text",
+    "end": "End time text",
+    "description": "description",
+    "fromNow": "Count from now"
+  }
+]
+*/
+const generateFlex = events => {
+  return {
+    type: 'flex',
+    altText: 'กิจกรรม/Events',
+    contents: { type: 'carousel', contents: events.map(e => eventTemplate(e))}
+  }
+}
+
+const generatePlainText = events => {
+
+  return {
+    type: 'text',
+    text: events.map(e => `*${e.name}*\n${e.start} - ${e.end}\n${description}`).join('\n\n')
+  }
+}
+
+module.exports = generatePlainText;
