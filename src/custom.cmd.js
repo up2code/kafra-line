edd0649c1bcd020033c1d4ab10a2ce953bbb8943
Augsorn Chanklad <augsorn.cha@ascendcorp.com>
2019-07-08 11:40:31 +0700

    Fix greencard custom cmd

--- src/custom.cmd.js
+++ src/custom.cmd.js
@@ -10,7 +10,7 @@ module.exports = {
     'events': () => firebase.getNonWeeklyEvents().then(eventListTemplate),
     'weeklyevents': () => firebase.getWeeklyEvents().then(eventListTemplate),
     'greycards': () => poporingRepo.getCards('grey').then(data => itemByCategoryTemplate('Cheapest common cards', data)),
-    'greencards': () => poporingRepo.getCards('greencards').then(data => itemByCategoryTemplate('Cheapest uncommon cards', data)),
+    'greencards': () => poporingRepo.getCards('green').then(data => itemByCategoryTemplate('Cheapest uncommon cards', data)),
     'bluecards': () => poporingRepo.getCards('blue').then(data => itemByCategoryTemplate('Cheapest rare cards', data)),
     'trending': () => poporingRepo.getAllTrendingList().then(trendingTemplate),
     'materials': () => poporingRepo.getActiveItemPricesByCategory('Material').then(data => itemByCategoryTemplate('Material prices', data)),
